5f46961a6796d4c1d0df26fbc295d5fd5444629f
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-01-13 23:03:25 +1100

    Updated / Created Dropdown Component

---
+++ frontend/src/components/Common/Dropdown.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent, useState } from "react";
+
+export interface DropdownProps {
+  defaultValue?: string;
+  placeHolder?: string;
+  options: DropdownOption[];
+  onChange?: (value: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export interface DropdownOption {
+  value: string;
+  label: string;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+  defaultValue,
+  placeHolder,
+  options,
+  onChange,
+}) => {
+  const [selectedValue, setSelectedValue] = useState<string>(
+    defaultValue || ""
+  );
+
+  // Make a dropdown menu for the navbar
+  return (
+    <select
+      className="bg-tertiary text-white px-4 py-3 w-full rounded-lg"
+      value={selectedValue}
+      onChange={(e) => {
+        setSelectedValue(e.target.value);
+
+        if (onChange) {
+          onChange(e);
+        }
+
+        e.preventDefault();
+      }}
+    >
+      {placeHolder && (
+        <option value="" disabled hidden>
+          {placeHolder}
+        </option>
+      )}
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
