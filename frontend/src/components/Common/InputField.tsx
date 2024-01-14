99fe4ec770d9c34fec031530fbb8a880b8cb87a6
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-01-14 21:54:01 +1100

    Added Label to Input Field

--- frontend/src/components/Common/InputField.tsx
+++ frontend/src/components/Common/InputField.tsx
@@ -1,12 +1,14 @@
 import { ChangeEvent, useState } from "react";
 
 export interface InputProps {
+  label?: string;
   defaultValue?: string;
   placeHolder?: string;
   onChange?: (value: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const InputField: React.FC<InputProps> = ({
+  label,
   defaultValue,
   placeHolder,
   onChange,
@@ -16,21 +18,29 @@ const InputField: React.FC<InputProps> = ({
   );
 
   return (
-    <input
-      type="text"
-      className="bg-tertiary text-white px-4 py-3 w-full rounded-lg"
-      placeholder={placeHolder}
-      value={selectedValue}
-      onChange={(e) => {
-        setSelectedValue(e.target.value);
+    <div className="mb-[30px]">
+      {label && (
+        <p className="text-textGray font-poppins text-base font-500 my-[10px]">
+          {label}
+        </p>
+      )}
 
-        if (onChange) {
-          onChange(e);
-        }
+      <input
+        type="text"
+        className="bg-tertiary text-white px-4 py-3 w-full rounded-lg"
+        placeholder={placeHolder}
+        value={selectedValue}
+        onChange={(e) => {
+          setSelectedValue(e.target.value);
 
-        e.preventDefault();
-      }}
-    />
+          if (onChange) {
+            onChange(e);
+          }
+
+          e.preventDefault();
+        }}
+      />
+    </div>
   );
 };
 
