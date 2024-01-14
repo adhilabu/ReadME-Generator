cb23ea84263f24bcf23907c9ec74089dc2070e71
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-01-14 21:55:25 +1100

    Updated Generator Interface

--- frontend/src/components/Generator/GeneratorNavbar.tsx
+++ frontend/src/components/Generator/GeneratorNavbar.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import CustomButton from "../Common/CustomButton";
 
 const GeneratorNavbar = () => {
   const navigate = useNavigate();
@@ -30,20 +31,6 @@ const GeneratorNavbar = () => {
         {/* TODO: Add a Seperator Here */}
       </div>
     </div>
-  );
-};
-
-const CustomButton: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  return (
-    <button
-      className="gap-[15.6px] flex items-center justify-between 
-      bg-themeGreen text-white font-poppins text-md font-semibold px-7 py-3 rounded-lg
-      transition-all hover:bg-opacity-80"
-    >
-      {children}
-    </button>
   );
 };
 
--- frontend/src/components/Generator/GeneratorSidebar.tsx
+++ frontend/src/components/Generator/GeneratorSidebar.tsx
@@ -1,4 +1,9 @@
 import useSidebarQuery from "../../hooks/useGetSidebar";
+import Dropdown, { DropdownOption } from "../Common/Dropdown";
+import InputField from "../Common/InputField";
+import TemplateSelector, {
+  ITemplateSelectorOption,
+} from "../Common/TemplateSelector/TemplateSelector";
 
 const GeneratorSidebar = () => {
   // useGetSidebar, show all elements from all items
@@ -8,63 +13,96 @@ const GeneratorSidebar = () => {
   if (isFetching) return <div>Loading...</div>;
   if (!data) return <div>No data</div>;
 
-  //   alert(JSON.stringify(data));
-
-  [
+  const dropdownOptions = [
     {
-      title: "The ReadME Project",
-      description:
-        "The first and capstone project of the GitHub ReadME Project",
-      id: "1",
-      functions: [
-        {
-          name: "License",
-          macroName: "license",
-          variables: [
-            {
-              label: "License URL",
-              variableName: "licenseURL",
-              value: "https://choosealicense.com/licenses/mit/",
-              type: "Input",
-            },
-            {
-              label: "License Type",
-              variableName: "licenseType",
-              value: "MIT",
-              type: "Input",
-            },
-          ],
-        },
-      ],
+      label: "Home",
+      value: "home",
     },
-  ];
+    {
+      label: "About",
+      value: "about",
+    },
+    {
+      label: "Contact",
+      value: "contact",
+    },
+  ] as DropdownOption[];
+
+  const templateOptions = [
+    {
+      label: "Home",
+      value: "home",
+      description: "Home page",
+    },
+    {
+      label: "About",
+      value: "about",
+      description: "About page",
+    },
+    {
+      label: "Contact",
+      value: "contact",
+      description: "Contact page",
+    },
+  ] as ITemplateSelectorOption[];
 
   return (
-    <div>
-      {data &&
-        data.map((item) => (
-          <div className="bg-red-500 mb-4" key={item.id}>
-            <h1>Title: {item.title}</h1>
-            <h2>Description: {item.description}</h2>
-            <h3>ID: {item.id}</h3>
-            {item.functions.map((func, index) => (
-              <div key={index}>
-                <h4>Name: {func.name}</h4>
-                <h5>Macro Name: {func.macroName}</h5>
-                {func.variables &&
-                  func.variables.map((variable, index) => (
-                    <div key={index}>
-                      <h6>Label: {variable.label}</h6>
-                      <h6>Variable Name: {variable.variableName}</h6>
-                      <h6>Value: {variable.value}</h6>
-                      <h6>Type: {variable.type}</h6>
-                    </div>
-                  ))}
-              </div>
-            ))}
-          </div>
-        ))}
+    <div className="bg-primary rounded-2xl p-5 w-1/5 min-h-screen">
+      {/* letter spacing -0.15px */}
+      <p className="text-textGray font-poppins text-base font-500 mb-[10px]">
+        Block Category
+      </p>
+
+      <Dropdown
+        placeHolder="Select a category"
+        defaultValue="contact"
+        options={dropdownOptions}
+        // onChange={(e) => alert(e.target.value)}
+      />
+
+      <InputField
+        label="Search Element"
+        defaultValue="Name"
+        placeHolder="Enter a element to search for"
+        // onChange={(e) => alert(e.target.value)}
+      />
+
+      <TemplateSelector label="Drews Template" options={templateOptions} />
+      <TemplateSelector label="Drews Template" options={templateOptions} />
     </div>
+
+    // <div>
+    //   {data &&
+    //     data.map((item) => (
+    //       <div className="bg-red-500 mb-4" key={item.id}>
+    //         <h1>Title: {item.title}</h1>
+    //         <h2>Description: {item.description}</h2>
+    //         <h3>ID: {item.id}</h3>
+    //         {item.functions.map((func, index) => (
+    //           <div key={index}>
+    //             <h4>Name: {func.name}</h4>
+    //             <h5>Macro Name: {func.macroName}</h5>
+    //             {func.variables &&
+    //               func.variables.map((variable, index) => (
+    //                 <div key={index}>
+    //                   <h6>Label: {variable.label}</h6>
+    //                   <h6>Variable Name: {variable.variableName}</h6>
+    //                   <h6>Value: {variable.value}</h6>
+    //                   <h6>Type: {variable.type}</h6>
+    //                 </div>
+    //               ))}
+    //           </div>
+    //         ))}
+
+    //         <button
+    //           className="bg-blue-500"
+    //           onClick={() => alert(JSON.stringify(item))}
+    //         >
+    //           Add Function
+    //         </button>
+    //       </div>
+    //     ))}
+    // </div>
   );
 };
 
